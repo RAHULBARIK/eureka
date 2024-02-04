d7a60e53a1b079bcfc2d3d4bd591bf3471c9a000
Rahul kumar barik <rahulbarik583@gmail.com>
2024-02-04 11:48:57 +0530

    Setting up the Eureka-server

---
+++ src/test/java/com/foodbasket/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodbasket.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
